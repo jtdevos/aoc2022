91a10383216302e426ce222a823c79c1945c2ab4
Jim deVos <jtdevos@asu.edu>
2022-12-09 23:08:44 -0700

    cleanup, logging

--- python3/day09/day09.py
+++ python3/day09/day09.py
@@ -1,4 +1,9 @@
 import os, math
+
+def debug(msg):
+    pass
+    #print(msg)
+
 def cls():
     os.system('cls||clear')
 
@@ -73,7 +78,7 @@ def test1():
     for x in range(-10, 10):
         for y in range(-10, 10):
             p = (x,y)
-            print(f'orig:{p}\t clamp:{clamp_tuple(p)}')
+            debug(f'orig:{p}\t clamp:{clamp_tuple(p)}')
 
 def code_to_vector(code):
     dct = {
@@ -91,39 +96,16 @@ def test2():
     pt = Point()
     ph.move(1,1)
     ph.move(-1,1)
-    print(f'Point ph: {ph.pos()}')
-    print(f'Point ph history: {ph.pos_history}')
+    debug(f'Point ph: {ph.pos()}')
+    debug(f'Point ph history: {ph.pos_history}')
     
-def part1(commands):
-    ph = Point()
-    pt = Point()
-    for code, rpt in commands:
-         v = code_to_vector(code)
-         print(f"Command:{code}->{v}\t times:{rpt}")
-         # move in specified direction rpt times
-         for i in range(rpt):
-            ph.move(v[0], v[1])
-            print(f"  ph:{ph}\t pt:{pt}\t dist:{ph.diff(pt).dist()}")
-            if(ph.diff(pt).dist()) >= 2:
-                print(f"    Distance too great -- scooting tail")
-                scootVec = ph.diff(pt).clamp()
-                pt.move(scootVec.x, scootVec.y)
-                print(f"    New tail position:{pt} (distance:{ph.diff(pt).dist()})")
-    print("*"*20)
-    print(f"Tail history:\t {pt.pos_history}")
-    tc = set(pt.pos_history)
-
-    print(f"Tail coverage:\t {tc}")
-    print(f"count of spaces covered: {len(tc)}")
-
-
 def run_commands(commands, chainsize):
     pchain = [Point() for _ in range(chainsize)]
     ph = pchain[0]
     pt = pchain[-1]
     for code, rpt in commands:
          v = code_to_vector(code)
-         print(f"Command:{code}->{v}\t times:{rpt}")
+         debug(f"Command:{code}->{v}\t times:{rpt}")
          # move in specified direction rpt times
          for _ in range(rpt):
             # move the absolute head, then determine if we need to move subsequent tails
@@ -131,29 +113,23 @@ def run_commands(commands, chainsize):
             for i in range(1, chainsize):
                 tph = pchain[i-1]
                 tpt = pchain[i]
-                print(f"  segment[{i-1}]:{tph}\t segment[{i}]:{tpt}\t dist:{tph.diff(tpt).dist()}")
+                debug(f"  segment[{i-1}]:{tph}\t segment[{i}]:{tpt}\t dist:{tph.diff(tpt).dist()}")
                 if(tph.diff(tpt).dist()) >= 2:
-                    print(f"    Distance too great -- scooting tail")
+                    debug(f"    Distance too great -- scooting tail")
                     scootVec = tph.diff(tpt).clamp()
                     tpt.move(scootVec.x, scootVec.y)
-                    print(f"    New tail position:{tpt} (distance:{tph.diff(tpt).dist()})")
-    print("*"*20)
-    print(f"Tail history:\t {pt.pos_history}")
+                    debug(f"    New tail position:{tpt} (distance:{tph.diff(tpt).dist()})")
+    debug("*"*20)
+    debug(f"Tail history:\t {pt.pos_history}")
     tc = set(pt.pos_history)
 
-    print(f"Tail coverage:\t {tc}")
+    debug(f"Tail coverage:\t {tc}")
     print(f"count of spaces covered: {len(tc)}")
-
-
-
-                
-
-
 
 
 def main(path):
     cmds = parse_commands(path)
-    print(cmds)
+    debug(cmds)
     # part 1
     run_commands(cmds, 2)
     # part 2
